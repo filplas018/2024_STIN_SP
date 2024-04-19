48da165477a5b4a66e89fd277c7754f60c24869f
Josef Debnár <info@josefdebnar.cz>
2024-04-19 17:38:36 +0200

    fix

--- resources/js/Layouts/Navbar.svelte
+++ resources/js/Layouts/Navbar.svelte
@@ -1,16 +1,5 @@
 <script>
-    import { onMount } from 'svelte';
-    import { user } from './stores'; // Assuming you have a store for user information
-
-    let userInfo = {};
-
-    onMount(async () => {
-        // Fetch user information from the backend
-        const response = await fetch('/user-info');
-        if (response.ok) {
-            userInfo = await response.json();
-        }
-    });
+    export let user;
 </script>
 
 <nav class="bg-gray-800 p-4">
@@ -18,8 +7,8 @@
         <div class="flex justify-between items-center">
             <h1 class="text-white text-2xl font-bold">My Website</h1>
             <div>
-                {#if $user.loggedIn}
-                    <p class="text-white">Welcome, {userInfo.name}</p>
+                {#if user}
+                    <p class="text-white">Welcome, {user}</p>
                     <!-- Add logout button or other user actions -->
                 {:else}
                     <a href="/login" class="text-white mr-4">Login</a>
